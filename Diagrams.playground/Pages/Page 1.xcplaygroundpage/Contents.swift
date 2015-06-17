77114779396e4e50a718e259ef73384b7c1935a7
al_skipp <al_skipp@fastmail.fm>
2015-06-17 09:10:48 +0100

    Minor formatting amend

--- Diagrams.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
+++ Diagrams.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
@@ -18,5 +18,6 @@ let ring = diagramRing(star, radius: 185, number: 12)
 let concentric = ring + .scale(x: 0.6, y: 0.6, ring) + .scale(x: 0.3, y: 0.3, ring)
 let diagram = concentric + .scale(x: 0.15, y: 0.15, concentric)
 
-showCoreGraphicsDiagram("Diagram", size: CGSize(width: 600, height: 500)) { drawDiagram(diagram)(context: $0)
+showCoreGraphicsDiagram("Diagram", size: CGSize(width: 600, height: 500)) {
+  drawDiagram(diagram)(context: $0)
 }
