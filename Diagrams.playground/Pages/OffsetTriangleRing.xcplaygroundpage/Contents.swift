1d4b4c64a407397e260297948ad1c70a8498a79f
al_skipp <al_skipp@fastmail.fm>
2015-06-19 19:16:37 +0100

    Tweaked triangle shape

--- Diagrams.playground/Pages/OffsetTriangleRing.xcplaygroundpage/Contents.swift
+++ Diagrams.playground/Pages/OffsetTriangleRing.xcplaygroundpage/Contents.swift
@@ -16,10 +16,10 @@ func iterateScale(s: CGFloat, offset: Point = (0,0), iterate: Int, _ diagram: Di
     diagram + translate(x: CGFloat(offset.0), y: CGFloat(offset.1), scale(x: s, y: s, diagram)))
 }
 
-let triangle = polygon( [(0, -25), (25, 0), (-25, 0)])
+let triangle = polygon([(0, -50), (25, 0), (-25, 0)])
 
 let triangleRing = ring(radius: 220, number: 27, triangle)
-let diagram = iterateScale(0.618, offset: (15, 30), iterate: 8, triangleRing)
+let diagram = iterateScale(0.618, offset: (15, 30), iterate: 7, triangleRing)
 
 showCoreGraphicsDiagram("Diagram", size: CGSize(width: 600, height: 500)) {
   drawDiagram(diagram)(context: $0)
